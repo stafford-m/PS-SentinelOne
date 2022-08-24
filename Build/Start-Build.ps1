aec6a5aa3ead2af90e0c20fac82247496ba26f2a
Chris Stone <chris@stoneyforest.net>
2022-08-24 11:21:02 -0400

    Moving to CI, 2

--- Build/Start-Build.ps1
+++ Build/Start-Build.ps1
@@ -4,12 +4,11 @@ param(
 
 # dependencies
 Get-PackageProvider -Name NuGet -ForceBootstrap | Out-Null
-if(-not (Get-Module -ListAvailable PSDepend))
-{
+If (-not (Get-Module -ListAvailable PSDepend)) {
 	& (Resolve-Path "$PSScriptRoot\helpers\Install-PSDepend.ps1")
 }
 Import-Module PSDepend
-$null = Invoke-PSDepend -Path "$PSScriptRoot\build.requirements.psd1" -Install -Import -Force
+Invoke-PSDepend -Path "$PSScriptRoot\build.requirements.psd1" -Install -Import -Force | Out-Null
 
 Set-BuildEnvironment -Force
 
